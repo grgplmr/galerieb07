4bfaea88214aab8b62b3f33835299bb7c52ce394
grgplmr <127120319+grgplmr@users.noreply.github.com>
2025-12-19 16:00:15 +0100

    Delete client-galleries directory

--- client-galleries/templates/front/email-gate.php
+++
@@ -1,8 +0,0 @@
-<div class="cg-email-gate">
-    <h3><?php esc_html_e('Please enter your email to view the gallery', 'client-galleries'); ?></h3>
-    <form id="cg-email-form">
-        <label for="cg-email-input" class="screen-reader-text"><?php esc_html_e('Email', 'client-galleries'); ?></label>
-        <input type="email" id="cg-email-input" required placeholder="<?php esc_attr_e('your@email.com', 'client-galleries'); ?>" />
-        <button type="submit"><?php esc_html_e('Access gallery', 'client-galleries'); ?></button>
-    </form>
-</div>
--- client-galleries/templates/front/thank-you.php
+++
@@ -1,3 +0,0 @@
-<div id="cg-thank-you" style="display:none;">
-    <p><?php esc_html_e('Thank you, your selection has been sent.', 'client-galleries'); ?></p>
-</div>
